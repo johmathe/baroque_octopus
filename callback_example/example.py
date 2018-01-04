17678be2cd4734a99185927f269309fbd0710139
Johan Mathe <johan.mathe@gmail.com>
2018-01-04 13:55:07 -0800

    update example

--- callback_example/example.py
+++ callback_example/example.py
@@ -4,7 +4,21 @@ matplotlib.use('Agg')
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.figure()
-x = np.arange(0.0, 5.0, 0.1)
-my_lib.solve(x, plt.plot)
-plt.savefig('result.png')
+class Viz:
+    def __init__(self):
+        self.debug_values = []
+
+    def callback(self, param):
+        self.debug_values.append(param)
+
+    def plot(self):
+        plt.figure()
+        for data in self.debug_values:
+            plt.plot(data)
+        plt.savefig('result.png')
+
+
+if __name__ == '__main__':
+    viz = Viz()
+    my_lib.solve(viz.callback)
+    viz.plot()
