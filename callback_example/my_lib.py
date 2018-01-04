9907dc9f513b4bb841ed133aa4d5de12ac112aa2
Johan Mathe <johan.mathe@gmail.com>
2018-01-04 13:55:40 -0800

    update callback

--- callback_example/my_lib.py
+++ callback_example/my_lib.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-def solve(x, plot_callback=None):
-    y = np.sin(x)
-    if plot_callback is not None:
-        plot_callback(x, y)
+N_ITER = 5
+
+def solve(plot_callback=None):
+    x = np.arange(0.0, 5.0, 0.1)
+    for i in range(N_ITER):
+        y = np.sin(i*x)
+        if plot_callback is not None:
+            plot_callback(y)
